98ee4b7a751c4256f2dc4c4bb1b36638eafd4335
nullagent07 <platonsolovyanov@gmail.com>
2025-02-20 18:13:58 +0300

    Add base application form page with standard configuration
    - Create new base-form page using ApplicationFormBlock
    - Configure fullscreen mode and standard application type
    - Import necessary global styles for consistent layout

---
+++ app/base-form/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import ApplicationFormBlock from '@/components/main/ApplicationFormBlock'
+import '../styles/variables.css'
+import '../styles/global.css'
+import '../styles/fonts.css'
+
+export default function Application() {
+  return (
+    <main>
+      <ApplicationFormBlock 
+        title="ОСТАВИТЬ ЗАЯВКУ"
+        applicationType="standard"
+        fullscreen
+      />
+    </main>
+  )
+}
